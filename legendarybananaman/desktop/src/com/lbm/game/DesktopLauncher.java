9c9a354f443a512f23d22628c2c7a30ba29da286
jochi2827 <jochi2827@BFBY082.Student.UGDSB.ED>
2023-12-11 10:30:28 -0500

    setup project, main menu screen (no features or appearance), added icon and is overall ready

--- legendarybananaman/desktop/src/com/lbm/game/DesktopLauncher.java
+++ legendarybananaman/desktop/src/com/lbm/game/DesktopLauncher.java
@@ -2,14 +2,16 @@ package com.lbm.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.lbm.game.LBM;
+import javax.swing.ImageIcon;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+                config.setResizable(false);
 		config.setForegroundFPS(60);
 		config.setTitle("Legendary Banana Man");
+                config.setWindowIcon("bananaman1.png");
 		new Lwjgl3Application(new LBM(), config);
 	}
 }
